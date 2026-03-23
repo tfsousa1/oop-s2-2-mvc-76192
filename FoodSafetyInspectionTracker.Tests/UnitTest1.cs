724db8944a560b2e7e819d85fcac0cadaac0a4c8
tfsousa1 <tamires.fs@gmail.com>
2026-03-23 21:26:32 +0000

    Initial project setup with MVC test project and CI workflow

---
+++ FoodSafetyInspectionTracker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodSafetyInspectionTracker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ FoodSafetyInspectionTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FoodSafetyInspectionTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
