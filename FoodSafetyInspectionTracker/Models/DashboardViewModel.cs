2b00dd7aad09229f9dd097ea6b5085fab36088f5
tfsousa1 <tamires.fs@gmail.com>
2026-03-30 23:25:48 +0100

    Improve dashboard filters, monthly metrics, and reporting view

--- FoodSafetyInspectionTracker/Models/DashboardViewModel.cs
+++ FoodSafetyInspectionTracker/Models/DashboardViewModel.cs
@@ -4,11 +4,16 @@
     {
         public int TotalPremises { get; set; }
         public int TotalInspections { get; set; }
+        public int InspectionsThisMonth { get; set; }
+        public int FailedInspectionsThisMonth { get; set; }
         public int OpenFollowUps { get; set; }
         public int OverdueFollowUps { get; set; }
 
         public string? SelectedTown { get; set; }
+        public string? SelectedRiskRating { get; set; }
+
         public List<string> Towns { get; set; } = new();
+        public List<string> RiskRatings { get; set; } = new();
 
         public List<Inspection> RecentInspections { get; set; } = new();
     }
